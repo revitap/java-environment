90dd51c95ecc188f63c957055316439a6511a6d2
Anthony <anthony.poleski@gmail.com>
2021-07-01 12:15:38 -0500

    adding Day 4 labs to repository

---
+++ HelloWorld/src/Hello.java
@@ -0,0 +1,12 @@
+
+public class Hello {
+	static String greeting = "Hello, everyone.";
+	
+	public static void myGreeting() {
+		System.out.println(greeting);		
+	}
+
+	public static void main(String[] args) {
+		myGreeting();
+	}	
+}
