e81fe5229ba157c4f14329f585ea594875cff3c0
Anthony <anthony.poleski@gmail.com>
2021-07-02 18:10:35 -0500

    add Project A

--- UnitConverter/src/main/Converter.java
+++ UnitConverter/src/main/Converter.java
@@ -4,17 +4,21 @@ import java.util.Scanner;
 
 public class Converter {
 
-	public static void main(String[] args) {
-		
-		Scanner input = new Scanner(System.in);		
+	public static void main(String[] args) {		
+			
 		int menuSelection = 0;
+		Scanner input = new Scanner(System.in);	
 		
 		while (menuSelection != 4) {
-			System.out.println("Please select an option:\n");
-			System.out.print("1. Cups To Teaspoons\n");
-			System.out.print("2. Miles To Kilometers\n");
-			System.out.print("3. US Gallons to Imperial Gallons\n");
-			System.out.print("4. Quit");
+			System.out.println("***************************");
+			System.out.println("UNIT CONVERTER APPLICATION");
+			System.out.println("***************************");
+			System.out.println("Please select an option:");
+			System.out.println("1. Cups To Teaspoons");
+			System.out.println("2. Miles To Kilometers");
+			System.out.println("3. US Gallons to Imperial Gallons");
+			System.out.println("4. Quit");
+			System.out.println("Enter option: ");
 			
 			menuSelection = input.nextInt();
 			
@@ -22,29 +26,32 @@ public class Converter {
 			
 			case 1: 
 				// Cups to Teaspoons
-				int cupNum, tspNum;
+				int cupNum;
+				double tspNum;
 				System.out.println("Please enter number of cups: ");
 				cupNum = input.nextInt();
 				tspNum = cupNum * 48;
-				System.out.println(cupNum + " cups is equal to " + tspNum + " teaspoons\n");
+				System.out.println(cupNum + " cup(s) is equal to " + tspNum + " teaspoons.\n");
 				break;
 				
 			case 2:
 				// Miles to Kilometers
-				double mileNum, kilometerNum;
+				int mileNum;
+				double kilometerNum;
 				System.out.println("Please enter number of miles: ");
 				mileNum = input.nextInt();
 				kilometerNum = mileNum * 1.609344;
-				System.out.println(mileNum + " miles is equal to " + kilometerNum + " kilometers\n");
+				System.out.println(mileNum + " mile(s) is equal to " + kilometerNum + " kilometers.\n");
 				break;
 				
 			case 3:
 				//US Gallons to Imperial Gallons
-				double usgNum, igNum;
+				int usgNum;
+				double igNum;
 				System.out.println("Please enter number of US gallons: ");
 				usgNum = input.nextInt();
 				igNum = usgNum * 0.83267382;
-				System.out.println(usgNum + " US gallons is equal to " + igNum + " Imperial gallons\n");
+				System.out.println(usgNum + " US gallon(s) is equal to " + igNum + " Imperial gallons.\n");
 				break;
 				
 			case 4:
@@ -52,7 +59,7 @@ public class Converter {
 				break;
 				
 			default:
-				System.out.println("Invalid option. Please select options 1 through 4.");			
+				System.out.println("Invalid option. Please make another selection.\n");			
 			}
 		}
 	}
