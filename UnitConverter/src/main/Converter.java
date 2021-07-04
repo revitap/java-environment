2a9eb4b4350444a29a684597d60880806d65a8c1
revitap <anthony.poleski@revature.net>
2021-07-04 13:02:04 -0500

    re-add UnitConverter

--- UnitConverter/src/main/Converter.java
+++ UnitConverter/src/main/Converter.java
@@ -3,12 +3,11 @@ package main;
 import java.util.Scanner;
 
 public class Converter {
-
-	public static void main(String[] args) {		
-			
+	
+	public static void main(String[] args) {
 		int menuSelection = 0;
-		Scanner input = new Scanner(System.in);	
-		
+		Scanner input = new Scanner(System.in);
+
 		while (menuSelection != 4) {
 			System.out.println("***************************");
 			System.out.println("UNIT CONVERTER APPLICATION");
@@ -19,12 +18,12 @@ public class Converter {
 			System.out.println("3. US Gallons to Imperial Gallons");
 			System.out.println("4. Quit");
 			System.out.println("Enter option: ");
-			
+
 			menuSelection = input.nextInt();
-			
-			switch(menuSelection) {
-			
-			case 1: 
+
+			switch (menuSelection) {
+
+			case 1:
 				// Cups to Teaspoons
 				int cupNum;
 				double tspNum;
@@ -33,7 +32,7 @@ public class Converter {
 				tspNum = cupNum * 48;
 				System.out.println(cupNum + " cup(s) is equal to " + tspNum + " teaspoons.\n");
 				break;
-				
+
 			case 2:
 				// Miles to Kilometers
 				int mileNum;
@@ -43,9 +42,9 @@ public class Converter {
 				kilometerNum = mileNum * 1.609344;
 				System.out.println(mileNum + " mile(s) is equal to " + kilometerNum + " kilometers.\n");
 				break;
-				
+
 			case 3:
-				//US Gallons to Imperial Gallons
+				// US Gallons to Imperial Gallons
 				int usgNum;
 				double igNum;
 				System.out.println("Please enter number of US gallons: ");
@@ -53,14 +52,16 @@ public class Converter {
 				igNum = usgNum * 0.83267382;
 				System.out.println(usgNum + " US gallon(s) is equal to " + igNum + " Imperial gallons.\n");
 				break;
-				
+
 			case 4:
 				System.out.println("Quit selected. Goodbye.");
 				break;
-				
+
 			default:
-				System.out.println("Invalid option. Please make another selection.\n");			
+				System.out.println("Invalid option. Please make another selection.\n");
 			}
 		}
 	}
+
 }
+
