a6ca69d55e7f8b9bf725253252ae7a809653b077
revitap <anthony.poleski@revature.net>
2021-07-09 08:28:50 -0500

    adding Project B: HomeTour update

--- HomeTour/src/gameobjects/Treasure.java
+++ HomeTour/src/gameobjects/Treasure.java
@@ -5,9 +5,6 @@ package gameobjects;
 public class Treasure extends Thing implements java.io.Serializable{
     /*
     Subclass of Thing class.
-    Treasure adds on an int value and a get accessor but not a set
-    accessor since the value of each Treasure never varies - it is
-    set just once when the object is constructed.
     */
 
     private int value;
